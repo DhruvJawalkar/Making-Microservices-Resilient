0bc82d267ba5c2ec00fc7efd61306daf434dc56e
Dhruv Jawalkar <dhruv.jawalkar@gmail.com>
2024-09-17 12:35:35 -0700

    Circuit Breaker pattern implementation -> Added base project code with Accounts (Accounts microservice), Config Server (Spring Cloud Configuration server), Eureka Server (Netflix Eureka server for Service Discovery of microservice in project) and Gateway Server (Spring Cloud API Gateway server for the edge server/ entrypoint into our microservices).

---
+++ Circuit Breaker pattern/README.md
@@ -0,0 +1,12 @@
+# Circuit Breaker pattern implementation
+
+## Base project structure
+- Modules in project are Accounts (Accounts microservice), Config Server (Spring Cloud Configuration server), Eureka Server (Netflix Eureka server for Service Discovery of microservice in project) and Gateway Server (Spring Cloud API Gateway server for the edge server/ entrypoint into our microservices).   
+
+## Bringing up the services
+- Run configserver
+- Run eurekaserver
+- Run accounts microservice
+- Run gatewayserver
+
+
---
+++ Circuit Breaker pattern/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,22 @@
+package com.eazybytes.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+				.defaultIfEmpty("anonymous");
+	}
+}
